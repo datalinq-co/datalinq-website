89bc8c1ced76c3c20e6d87c453813420db50a35d
Alvvion <alvvion@gmail.com>
2022-07-03 17:46:13 +0530

    Faq Fix

--- src/containers/Faq.js
+++ src/containers/Faq.js
@@ -1,14 +1,17 @@
-import React from "react";
-import Faq from "../components/Faq.js";
+import React from "react"
+import Faq from "../components/Faq.js"
+import faq from "../../site_cms/faqs.json"
 
 export default function FaqContainer() {
-    return (
-        <Faq>
-            <Faq.Title>Frequently Asked Question</Faq.Title>
-            <Faq.Item>
-                <Faq.Header>Header for the Faq</Faq.Header>
-                <Faq.Body>Body for the Faq</Faq.Body>
-            </Faq.Item>
-        </Faq>
-    )
+  return (
+    <Faq>
+      <Faq.Title>Frequently Asked Question</Faq.Title>
+      {faq.list.map(ele => {
+        ;<Faq.Item>
+          <Faq.Header>{ele.header}</Faq.Header>
+          <Faq.Body>{ele.body}</Faq.Body>
+        </Faq.Item>
+      })}
+    </Faq>
+  )
 }
